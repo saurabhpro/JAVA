a7e8fe46e00890ee35b3e8e00398619eb30d0599
Saurabh Kumar <saurabhk1511@gmail.com>
2015-08-31 02:09:53 +0530

    Inner Class done
    n

---
+++ JavaCon/InnerClass/src/AnonymousClass.java
@@ -0,0 +1,83 @@
+/**
+ * Created by Saurabh on 8/30/2015.
+ */
+
+/**
+ * While local classes are class declarations, anonymous classes are expressions,
+ * which means that you define the class in another expression.
+ * <p>
+ * while local classes are class declarations, anonymous classes are expressions,
+ * which means that you define the class in another expression..
+ * <p>
+ * The anonymous class expression consists of the following:
+ * <p>
+ * <ul>
+ * <li>The new operator</li>
+ * <li>The name of an interface to implement or a class to extend. In this example,
+ * the anonymous class is implementing the interface HelloWorld.</li>
+ * <li>Parentheses that contain the arguments to a constructor, just like a normal class instance creation expression.
+ * Note: When you implement an interface, there is no constructor, so you use an empty pair of parentheses, as in this example.</li>
+ * <li>A body, which is a class declaration body. More specifically, in the body, method declarations are allowed but statements are not.</li>
+ * <li> Because an anonymous class definition is an expression, it must be part of a statement.</li>
+ * </ul>
+ * <p>
+ */
+
+
+public class AnonymousClass {
+
+    public static void main(String[] args) {
+        AnonymousClass aOb = new AnonymousClass();
+        aOb.doStuff();
+    }
+
+    void doStuff() {
+
+        class Popcorn {
+            void pop() {
+                System.out.println("Poped PopCorn");
+            }
+        }
+
+        class Food {
+            Popcorn p = new Popcorn() {
+                @Override
+                void pop() {
+                    System.out.println("Popped anonymous popcorn!!");
+                } //overides base class Popcorn's pop method
+            };
+
+            /**
+             * here we are creating a subclass of Cookable which is basically an implementer class
+             */
+
+            // since java 8 we can use Lambda expressions to provide an implementation for interface via anonymous class
+            // Cookable c = () -> System.out.println("Anonymous cookable implementer!!");         //one function only
+
+            Cookable c = new Cookable() {
+                @Override
+                public void cook() {
+                    System.out.println("Anonymous cookable implementer!!");
+                }//implements method cook if interface cookable
+
+                @Override
+                public void wash() {
+                    System.out.println("Anonymous Wash in");
+                }
+            };
+        }
+        Food fOb = new Food();
+        fOb.p.pop();
+
+        fOb.c.cook();
+        fOb.c.wash();
+
+    }
+
+
+    interface Cookable {
+        void cook();
+
+        void wash();
+    }   //interfaces method ke scope me nhi likh skte
+}
---
+++ JavaCon/InnerClass/src/OuterSimpleInner.java
@@ -0,0 +1,121 @@
+/**
+ * Created by Saurabh on 8/30/2015.
+ */
+
+/**
+ * You can use inner classes to implement helper classes. To handle user interface events,
+ * you must know how to use inner classes, because the event-handling mechanism makes extensive use of them.
+ */
+public class OuterSimpleInner {
+    private static int n = 9;
+
+    static {
+        System.out.println("static block of outer");
+    }
+
+    private int m = 10;
+
+    {
+        System.out.println("Init of outer");
+    }
+
+    private static void show() {
+        System.out.println("OuterSimpleInner Static method");
+    }
+
+    public static void main(String[] args) {
+        OuterSimpleInner ob = new OuterSimpleInner();
+        OuterSimpleInner.Inner oi = ob.new Inner();
+        oi.show();
+
+
+        MyOuter out = new MyOuter();
+
+        out.makeInner();            //either have a function in MyOuter class which makes the object
+        //or
+        MyOuter.MyInner inr = out.new MyInner();    //do it from here
+        //or in one line
+        MyOuter.MyInner in2 = new MyOuter().new MyInner();
+        inr.seeOuter();
+    }
+
+    public void display() {
+        System.out.println("OuterSimpleInner Non-Static method");
+    }
+
+    private class Inner {
+        //    static int x = 99;        //cannot have static declaration in [non-static] inner class
+        //static (with final) is allowed in Inner classes as they combined with final make constant value
+        static final int z = 66;
+        int y = 110;
+
+        // static {    int y = 110;     }
+        {
+            System.out.println("Inside init of inner");
+        }
+
+        Inner(OuterSimpleInner s) {
+            System.out.println("Inside Inner para constructor");
+        }
+
+        Inner() {
+            System.out.println("Inside Inner non para constructor");
+        }
+
+        void show() {
+            System.out.println(n + " " + m);        //static variable from outer = n allowed
+            System.out.println("OuterSimpleInner class reference " + OuterSimpleInner.this);
+            System.out.println("Inner class reference " + this);
+
+            OuterSimpleInner.this.display();       //only display wont work as it'll look for this.display() which isnt there!
+            OuterSimpleInner.show();   //or OuterSimpleInner.this.show();
+        }
+    }
+
+}
+
+class MyOuter {
+    private int i = 7;
+
+    /**
+     * if we make this function static , we cannot create the instance of inner here as MyOuter.this wont work
+     * inside static field there is no this, similarly getClass() wont work as non-static function
+     */
+    public void makeInner() {
+
+        System.out.println("\n\n" + getClass());     //this.getClass())
+
+        //Inner class instances are always handed an implicit reference of outer class
+        MyInner in = new MyInner();             //implicitly  MyOuter.MyInner in = new MyOuter.MyInner();
+        in.seeOuter();
+        System.out.println(in.getClass());      //this ke badle in
+    }
+
+    class MyInner {
+        public void seeOuter() {
+            System.out.println("OuterSimpleInner x is : " + i);      //  MyOuter.this.i
+
+            System.out.println(getClass());             //this.getClass())
+
+        }
+    }
+
+    //all these are allowed for normal inner classes
+    public static class inner2 {
+    }        //this basically is nested class
+
+    private abstract class inner3 {
+    }
+
+    private final class inner4 {
+    }
+
+    protected strictfp class inner5 {
+    }
+
+
+    //Inner class are fine , what about inner interfaces
+    interface x {
+    }   //valid
+
+}
