8b7efc822f65bc211e018103efb02302e0b092f4
Saurabh Kumar <saurabhk1511@gmail.com>
2016-12-17 10:53:22 +0530

    static block RIWO state and init block

--- JavaCon/SpecialKeywords/src/StaticFamily.java
+++ JavaCon/SpecialKeywords/src/StaticFamily.java
@@ -2,4 +2,24 @@
  * Created by Saurabh on 8/17/2015.
  */
 public class StaticFamily {
+	static int i = 10;
+	static int j = 20;
+
+	static {
+		m1();
+		System.out.println("first block");
+	}
+
+	static {
+		System.out.println("second block");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Main method");
+	}
+
+	public static void m1() {
+		System.out.println(j);
+	}
+
 }
