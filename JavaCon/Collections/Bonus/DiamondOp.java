ada83cec42274b1d1ccfe7afaaeba1f541325daf
Saurabh Kumar <saurabhk1511@gmail.com>
2017-01-21 22:23:23 +0530

    diamondoperator and singleton class

---
+++ JavaCon/Collections/Bonus/DiamondOp.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+/**
+ * Created by Saurabh on 1/21/2017.
+ * <p>
+ * Generics is implemented via Erasure.
+ * Type parameters in generics are for compile time safety.
+ * Once compiled at bytecode level, raw and generic types are same.
+ * Compile erases all the type information at bytecode level.
+ * It means bytecode of List<String> is same as bytecode of List.
+ */
+
+public class DiamondOp {
+	public static void main(String[] args) {
+		RightSideDiamondSkipWorks.test();
+
+	}
+
+	static class RightSideDiamondSkipWorks {
+		static void test() {
+			//String s = new Integer(4); doesnt work - the how come this worked ?
+			ArrayList test = new ArrayList<>();
+			test.add(new Integer(1));   //even though the method expects ArrayList of strings
+
+			test.add("Hi");
+			RightSideDiamondSkipWorks obj = new RightSideDiamondSkipWorks();
+			obj.testMethod1(test);
+		}
+
+		//proper type required in args always , cannot use <> here
+		void testMethod1(ArrayList<String> argStringArrayList) {
+			System.out.println("I am String list");
+
+			// #1
+			Arrays.asList(argStringArrayList).stream().forEach(s -> System.out.println(s));
+			//or
+			Collections.singletonList(argStringArrayList).stream().forEach(System.out::println);
+
+			// #2 also works for array
+			Stream.of(argStringArrayList).forEach(System.out::println);
+
+			// #3 Error for arrList
+			//Arrays.stream(expectArrayAsNoListTransformDone).forEach(System.out::println);
+
+			// #4 only sout also works for list but not for arrays
+			System.out.println(argStringArrayList);
+		}
+
+		void testMethod2(ArrayList<Integer> argIntegerArrayList) {
+			System.out.println("I am Integer list");
+			Stream.of(argIntegerArrayList).forEach(System.out::println);
+		}
+	}
+
+	class ErasureProblemsWithGenerics {
+	/*	void testMethod1(ArrayList<String> argStringArrayList){
+			System.out.println("I am String list");
+		}
+
+		void testMethod1(ArrayList<Integer> argIntegerArrayList){
+			System.out.println("I am Integer list");
+		}
+
+		HENCE:-
+
+ * Error:(12, 14) java: name clash: testMethod1(java.util.ArrayList<java.lang.Integer>)
+ * and testMethod1(java.util.ArrayList<java.lang.String>) have the same erasure
+	*/
+	}
+
+}
+
