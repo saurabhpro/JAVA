1e10d027ec0cb0c3dfc06e768ddc8d7f0ba43d53
Saurabh Kumar <saurabhk1511@gmail.com>
2017-09-15 06:46:07 +0530

    basics of sending mail with spring boot

---
+++ SpringHibernateAndWebDevelopment/C_BasicSpringBoot/gmail_send_mail/src/main/java/com/saurabh/SimpleEmailController3.java
@@ -0,0 +1,45 @@
+package com.saurabh;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.mail.internet.MimeMessage;
+
+@Controller
+public class SimpleEmailController3 {
+
+	@Autowired
+	private JavaMailSender sender;
+
+	@RequestMapping("/simpleemail3")
+	@ResponseBody
+	String home() {
+		try {
+			sendEmail();
+			return "Email Sent!";
+		} catch (Exception ex) {
+			return "Error in sending email: " + ex;
+		}
+	}
+
+	private void sendEmail() throws Exception {
+		MimeMessage message = sender.createMimeMessage();
+
+		// Enable the multipart flag!
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+		helper.setTo("saurabhk15@outlook.com");
+		helper.setText("<html><body>Here is a cat picture! <img src='cid:id101'/><body></html>", true);
+		helper.setSubject("Hi");
+
+		ClassPathResource file = new ClassPathResource("/static/img/cat.jpg");
+		helper.addInline("id101", file);
+
+		sender.send(message);
+	}
+}
