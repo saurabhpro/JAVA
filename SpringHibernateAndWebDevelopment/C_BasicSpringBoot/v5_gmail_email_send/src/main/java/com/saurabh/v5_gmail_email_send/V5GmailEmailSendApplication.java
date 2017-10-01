4c147b0ffcadf2209e941140b5a0e8606634a24f
Saurabh Kumar <saurabhk1511@gmail.com>
2017-10-01 15:47:46 +0530

    simple mails using sprinboot

--- SpringHibernateAndWebDevelopment/C_BasicSpringBoot/v5_gmail_email_send/src/main/java/com/saurabh/v5_gmail_email_send/V5GmailEmailSendApplication.java
+++ SpringHibernateAndWebDevelopment/C_BasicSpringBoot/v5_gmail_email_send/src/main/java/com/saurabh/v5_gmail_email_send/V5GmailEmailSendApplication.java
@@ -1,12 +1,12 @@
-package com.saurabh;
+package com.saurabh.v5_gmail_email_send;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailThymleafApplication {
+public class V5GmailEmailSendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmailThymleafApplication.class, args);
+		SpringApplication.run(V5GmailEmailSendApplication.class, args);
 	}
 }
---
+++ SpringHibernateAndWebDevelopment/C_BasicSpringBoot/v5_gmail_email_send/src/test/java/com/saurabh/v5_gmail_email_send/V5GmailEmailSendApplicationTests.java
@@ -0,0 +1,10 @@
+package com.saurabh.v5_gmail_email_send;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class V5GmailEmailSendApplicationTests {
+}
