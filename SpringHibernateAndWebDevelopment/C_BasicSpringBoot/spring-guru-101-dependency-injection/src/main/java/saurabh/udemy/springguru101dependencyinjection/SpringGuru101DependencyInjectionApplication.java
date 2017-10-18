add6a11a42253e6e105fa79350268407d0b85b6a
Saurabh Kumar <saurabhk1511@gmail.com>
2017-10-18 14:28:27 +0530

    uncommented used code

--- SpringHibernateAndWebDevelopment/C_BasicSpringBoot/spring-guru-101-dependency-injection/src/main/java/saurabh/udemy/springguru101dependencyinjection/SpringGuru101DependencyInjectionApplication.java
+++ SpringHibernateAndWebDevelopment/C_BasicSpringBoot/spring-guru-101-dependency-injection/src/main/java/saurabh/udemy/springguru101dependencyinjection/SpringGuru101DependencyInjectionApplication.java
@@ -3,6 +3,10 @@ package saurabh.udemy.springguru101dependencyinjection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import saurabh.udemy.springguru101dependencyinjection.controllers.ConstructorInjectedController;
+import saurabh.udemy.springguru101dependencyinjection.controllers.MyController;
+import saurabh.udemy.springguru101dependencyinjection.controllers.PropertyInjectedController;
+import saurabh.udemy.springguru101dependencyinjection.controllers.SetterInjectedController;
 import saurabh.udemy.springguru101dependencyinjection.model.FakeDataSource;
 import saurabh.udemy.springguru101dependencyinjection.model.FakeJmsBroker;
 
@@ -12,16 +16,17 @@ public class SpringGuru101DependencyInjectionApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SpringGuru101DependencyInjectionApplication.class, args);
 
-//		MyController controller = (MyController) ctx.getBean("myController");
-//
-//		controller.sayHello();
-//
-//		System.out.println(controller.sayHello());
-//
-//		// Marking of Spring bean is required since then only we can use it here
-//		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-//		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-//		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.sayHello();
+
+		System.out.println(controller.sayHello());
+
+		// Marking of Spring bean is required since then only we can use it here
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+
 
 		// Step 2: Make Class
 		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
