9ef22cf2cc13799ce5e256932e2272bc45eff1f5
Saurabh Kumar <saurabhk15@outlook.com>
2017-10-18 20:50:02 +0530

    cleanup

---
+++ SpringHibernateAndWebDevelopment/P1_Attendance/V2/v2-attendance/src/main/java/com/saurabh/v2attendance/V2AttendanceApplication.java
@@ -0,0 +1,12 @@
+package com.saurabh.v2attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2AttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2AttendanceApplication.class, args);
+	}
+}
