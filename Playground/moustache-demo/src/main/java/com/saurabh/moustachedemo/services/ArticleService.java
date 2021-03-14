7730a73fb79e4e2cac3f1fdc21b9d08da8e7ec0a
Saurabh Kumar <saurabh.kumar@cvent.com>
2021-03-14 18:30:07 +0530

    add modularization

--- Playground/moustache-demo/src/main/java/com/saurabh/moustachedemo/services/ArticleService.java
+++
@@ -1,9 +0,0 @@
-package com.saurabh.moustachedemo.services;
-
-import com.saurabh.moustachedemo.models.Article;
-
-import java.util.List;
-
-public interface ArticleService {
-    List<Article> getArticles();
-}
