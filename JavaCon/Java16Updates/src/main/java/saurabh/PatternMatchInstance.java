ea37e302cca6ecc932f3c092ddd27f9bc9f6544b
Saurabh Kumar <saurabh.kumar@cvent.com>
2021-04-08 01:28:10 +0530

    upgrade to jdk 16 adopt

---
+++ JavaCon/Java16Updates/src/main/java/saurabh/PatternMatchInstance.java
@@ -0,0 +1,19 @@
+package saurabh;
+
+public class PatternMatchInstance {
+    public static void main(String[] args) {
+        String obj = "";
+
+        // previously < 16
+        if (obj instanceof String) {
+        }
+
+//        if (obj instanceof String str) {
+//            // can use str here
+//            System.out.println("".equals(str));
+//        } else {
+//            // can't use str here
+//            System.out.println("Can't use str here");
+//        }
+    }
+}
---
+++ JavaCon/Java16Updates/src/main/java/saurabh/Point.java
@@ -0,0 +1,12 @@
+package saurabh;
+
+record PointRecord(int latitude, int longitude) {
+}
+
+public class Point {
+    public static void main(String[] args) {
+        var pointRecord = new PointRecord(5, 3);
+        System.out.println(pointRecord.latitude());
+        System.out.println(pointRecord.longitude());
+    }
+}
