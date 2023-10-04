a0e92ea4c4cabf535b844491c7da980092311b19
saurabh.kumar <saurabh.kumar@revolut.com>
2023-10-04 21:27:38 +0100

    first attempt jdk 21

--- JavaCon/JsonIO/src/main/java/com/saurabh/special/api/JsonReader.java
+++ JavaCon/JsonIO/src/main/java/com/saurabh/special/api/JsonReader.java
@@ -8,11 +8,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
@@ -22,41 +19,26 @@ import java.nio.charset.Charset;
 
 public class JsonReader {
 
-	@SuppressWarnings("TryWithIdenticalCatches")
-	public static <T> T getJSONObjectFromFileUsingJackson(String jsonFileName, Class<T> classForMapping) {
-		String data;
+    @SuppressWarnings("TryWithIdenticalCatches")
+    public static <T> T getJSONObjectFromFileUsingJackson(String jsonFileName, Class<T> classForMapping) {
+        String data;
 
-		//Create a new ObjectMapper, for mapping data to POJO
-		ObjectMapper mapper = new ObjectMapper();
+        //Create a new ObjectMapper, for mapping data to POJO
+        ObjectMapper mapper = new ObjectMapper();
 
-		try {
-			data = FileUtils.readFileToString(new File(jsonFileName), (Charset) null);
+        try {
+            data = FileUtils.readFileToString(new File(jsonFileName), (Charset) null);
 
-			return mapper.readValue(data, classForMapping);
+            return mapper.readValue(data, classForMapping);
 
-		} catch (JsonParseException e) {
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	@SuppressWarnings("unused")
-	public static JSONObject getJSONObjectFromFileUsingJsonSimple(String filePath) {
-
-		JSONParser parser = new JSONParser();
-
-		try {
-			Object obj = parser.parse(new FileReader(filePath));
-
-			return (JSONObject) obj; // problem - we still need to map it manually to our class
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
 }
--- JavaCon/JsonIO/src/main/java/com/saurabh/special/model/StudentDetails.java
+++ JavaCon/JsonIO/src/main/java/com/saurabh/special/model/StudentDetails.java
@@ -6,52 +6,38 @@ package com.saurabh.special.model;
 
 import java.util.List;
 
-public class StudentDetails {
-	private int id;
-	private String name;
-	private String department;
-	private List<String> favoriteSports;
+public record StudentDetails(int id,
+                             String name,
+                             String department,
+                             List<String> favoriteSports) {
+    public static class Builder {
+        private int id;
+        private String name;
+        private String department;
+        private List<String> favoriteSports;
 
-	public StudentDetails() {
-	}
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
 
-	public StudentDetails(int id, String name, String department,
-	                      List<String> favoriteSports) {
-		this.id = id;
-		this.name = name;
-		this.department = department;
-		this.favoriteSports = favoriteSports;
-	}
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
 
-	public int getId() {
-		return id;
-	}
+        public Builder setDepartment(String department) {
+            this.department = department;
+            return this;
+        }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+        public Builder setFavoriteSports(List<String> favoriteSports) {
+            this.favoriteSports = favoriteSports;
+            return this;
+        }
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
-	public List<String> getFavoriteSports() {
-		return favoriteSports;
-	}
-
-	public void setFavoriteSports(List<String> favoriteSports) {
-		this.favoriteSports = favoriteSports;
-	}
+        public StudentDetails build() {
+            return new StudentDetails(id, name, department, favoriteSports);
+        }
+    }
 }
