935b41d9b3d8bfb603e2e34dbca73f743f57b7a9
saurabh.kumar <saurabh.kumar@revolut.com>
2024-05-14 08:03:44 +0100

    jdk 22 path

---
+++ JavaCon/Java25Features/src/main/java/com/saurabh/java/UnnamedPatternVariable.java
@@ -0,0 +1,42 @@
+package com.saurabh.java;
+
+public class UnnamedPatternVariable {
+
+    record Car(String name, String color, String mileage) {
+
+        Car(String name) {
+            this(name, "Red", "100k");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        final var car = new Car("tesla");
+        try {
+            throw new IllegalArgumentException("test");
+        } catch (IllegalStateException _) {
+            System.out.println(STR."Got an illegal state exception for: \{car.name()}");
+        } catch (RuntimeException _) {
+            System.out.println("Got a runtime exception!");
+        }
+
+        final var color = getObjectsColorWithUnnamedPattern(car);
+        System.out.println(color);
+
+        System.out.println(getObjectsColorWithSwitchAndUnnamedPattern(car));
+    }
+
+    static String getObjectsColorWithUnnamedPattern(Object object) {
+        if (object instanceof Car(_, var color, _)) {
+            return color;
+        }
+        return "No color!";
+    }
+
+    static String getObjectsColorWithSwitchAndUnnamedPattern(Object object) {
+        return switch (object) {
+            case Car(_, var color, _) -> color;
+            default -> "No color!";
+        };
+    }
+}
